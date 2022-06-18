7e3f1a3f5d48affc97fa7bf7d1c9778652008955
tunlaton11 <tunlaton11@gmail.com>
2022-06-18 15:29:54 +0700

    remove vite

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,69 @@
+<template>
+  <section>
+    <img v-bind:src="picture" :width="size"/> <!--bind data with attribute using v-bind: or : -->
+    <h2>Fullname: {{getFullName()}}</h2>
+    <h2>Age: {{age}}</h2>
+    <h2>Address: <span v-html="address"></span></h2>
+    <a :href="social">facebook</a> <!--bind-->
+    <p>Hobby</p>
+    <ul>
+      <li>{{hobby[0]}}</li>
+      <li>{{hobby[1]}}</li>
+      <li>{{hobby[2]}}</li>
+    </ul>
+    <p>General</p>
+    <ul>
+      <li>Sex: {{general.sex}}</li>
+      <li>Height: {{general.height}}</li>
+      <li>Weight: {{general.weight}}</li>
+    </ul>
+    <button @click="showData()">Click</button> <!--event using v-on: or @-->
+    <button @click="inAge(2)">Increase Age</button>
+    <button @click='deAge()'>Decrease Age</button>
+  </section>
+</template>
+
 <script>
 
-import About from '/src/About.vue'
-import Home from '/src/Home.vue'
-
-const routes = {
-  '/': Home,
-  '/about': About
-}
 
 export default {
-  data() {
-    return {
-      currentPath: window.location.hash
+  name: 'App',
+  data(){ //keep attribute data
+    return{
+      fname: 'Tunlaton',
+      lname: 'Wongchai',
+      age: 20,
+      address: '<i>Bangkok</i>',
+      picture: 'https://play-lh.googleusercontent.com/nCVVCbeSI14qEvNnvvgkkbvfBJximn04qoPRw8GZjC7zeoKxOgEtjqsID_DDtNfkjyo',
+      size: 100,
+      social: 'https://www.facebook.com/tunlaton.wongchai',
+      hobby: ['music', 'travel', 'coffee'], // array
+      general: { // object
+        sex: 'Male',
+        height: 165,
+        weight: 55
+      }
+    
     }
   },
-  computed: {
-    currentView() {
-      return routes[this.currentPath.slice(1) || '/'] || NotFound
+
+  methods:{ //likely function
+    getFullName(){
+      return this.fname + ' ' + this.lname
+    },
+    showData(){
+      alert(this.fname + ' ' + this.lname)
+    },
+    inAge(num){
+      this.age += num
+    },
+    deAge(){
+      this.age -= 1
     }
-  },
-  mounted() {
-    window.addEventListener('hashchange', () => {
-		  this.currentPath = window.location.hash
-		})
   }
 }
 </script>
 
-<template>
-  <a href="#/">Home</a> |
-  <a href="#/about">About</a> |
-  <a href="#/non-existent-path">Broken Link</a>
-  <component :is="currentView" />
-</template>
+<style>
+
+</style>
